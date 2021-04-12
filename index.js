d1ab5f2ce2d35e3c72ece211f9646a32f685cb9b
Christopher Villaran <villaran.christopher@gmail.com>
2021-04-12 11:55:45 +0800

    hello world using express

---
+++ index.js
@@ -0,0 +1,8 @@
+const Express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+Express()
+  .use(Express.static(path.join(__dirname, 'client/dist')))
+  .get('/', (req, res) => res.render(path.join(__dirname, 'client/dist/index.html')))
+  .listen(PORT, () => console.log(`Listening on http://localhost:${ PORT }`))
