d658cd8e9df524d5d3bf8fb4c3b6ed219bc5b7cf
Christopher Villaran <villaran.christopher@gmail.com>
2021-04-12 12:04:15 +0800

    fixed express error on static files

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 5000
 
 Express()
   .use(Express.static(path.join(__dirname, 'client/dist')))
-  .get('/', (req, res) => res.render(path.join(__dirname, 'client/dist/index.html')))
+  .get('/', (req, res) => res.render(path.join(__dirname, 'index.html')))
   .listen(PORT, () => console.log(`Listening on http://localhost:${ PORT }`))
