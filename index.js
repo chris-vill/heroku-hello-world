e58fad12919d2795754e64bd43600657c101940f
Christopher Villaran <villaran.christopher@gmail.com>
2021-04-12 13:47:56 +0800

    cleaned index.js in server

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const Express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-Express()
-  .set('views', __dirname + '/client/dist')
-  .set('view engine', 'html')
-  .engine('html', require('ejs').renderFile)
-  .get('/', (req, res) => res.render('index.html'))
-  .listen(PORT, () => console.log(`Listening on http://localhost:${ PORT }`))
+const app = Express();
+
+app.set(
+  'views',
+  path.join(__dirname, '/client/dist')
+);
+app.engine(
+  'html',
+  require('ejs').renderFile
+);
+app.set(
+  'view engine',
+  'html'
+);
+
+// Endpoints
+app.get(
+  '/',
+  (req, res) => res.render('index.html')
+);
+
+// Port
+app.listen(
+  PORT,
+  () => console.log(`Listening on http://localhost:${ PORT }`)
+);
